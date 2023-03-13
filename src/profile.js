10c2f5a3c6f4d4913974600fcb33fd0b926cc643
huyhoangm2 <hoangken760@gmail.com>
2023-03-13 17:35:05 +0700

    sửa css

--- src/profile.js
+++ src/profile.js
@@ -6,7 +6,7 @@ import * as Yup from "yup";
 const ProfileSchema = Yup.object().shape({
   username: Yup.string()
     .min(2, "Tên của bạn phải có ít nhất 2 ký tự")
-    .max(50, "Tên của bạn không được dài hơn 50 ký tự")
+    .max(40, "Tên của bạn không được dài hơn 40 ký tự")
     .required("Vui lòng nhập tên người dùng"),
   email: Yup.string()
     .email("Email không hợp lệ")
@@ -16,7 +16,7 @@ const ProfileSchema = Yup.object().shape({
     .required("Vui lòng nhập số điện thoại"),
   password: Yup.string()
     .min(6, "Mật khẩu phải có ít nhất 6 ký tự")
-    .max(50, "Mật khẩu không được dài hơn 50 ký tự")
+    .max(30, "Mật khẩu không được quá 30 ký tự")
     .required("Vui lòng nhập mật khẩu"),
   address: Yup.string()
     .required("Địa chỉ không được bỏ trống"),
@@ -24,8 +24,11 @@ const ProfileSchema = Yup.object().shape({
 
 const ProfileForm = () => {
   return (
-    <div className={styles.container}>
+    <div >
+    {/* className={styles.container} */}
+
     <h1 className={styles.title}>PROFILE </h1>
+      <h2 className={styles.h} >General information</h2>
     <Formik
       initialValues={{
         name: "",
@@ -44,32 +47,45 @@ const ProfileForm = () => {
     >
       {({ isSubmitting }) => (
        
-        <Form >
+        <Form className={styles.hhh}>
             <tr>
+<td>
 
-          <p className={styles.khung}>User name </p>
+          <p className={styles.khung1}>User name </p>
           
-            <Field type="text" name="name" className={styles.input} />
+            <Field type="text" name="name" className={styles.input1} />
             <ErrorMessage name="username" component="div" className={styles.error} />
           
-          <p className={styles.khung}>Password </p>
+</td>
+<td>
+
+          <p className={styles.khung1}>Password </p>
           
             
-            <Field type="password"  name="password" className={styles.input} />
+            <Field type="password"  name="password" className={styles.input2} />
             <ErrorMessage name="password" component="div" className={styles.error} />
           
+</td>
             </tr>
+            <tr>
+<td>
+
           <p className={styles.khung}> Email</p>
         
-            <Field type="email"  name="email" className={styles.input} />
+            <Field type="email"  name="email" className={styles.input1} />
             <ErrorMessage name="email" component="div" className={styles.error} />
           
+          
+</td>
+<td>
           <p className={styles.khung}>Phone </p>
-          
-            
-            <Field type="text" name="phone" className={styles.input} />
+
+            <Field type="text" name="phone" className={styles.input2} />
             <ErrorMessage name="phone" component="div" className={styles.error} />
+</td>
+            
           
+            </tr>
           
           <p className={styles.khung}>Address </p>
         
